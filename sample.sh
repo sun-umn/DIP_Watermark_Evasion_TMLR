8ee7d529bfcb5aa81b75e8cdcc60d17085e4bc50
HengyueL <lianghengyue1993@outlook.com>
2024-04-10 16:23:28 -0500

    Update

---
+++ sample.sh
@@ -0,0 +1,11 @@
+
+python playgrounds/playground_single_image.py --evade_method vae
+python playgrounds/playground_single_image.py --evade_method dip
+python playgrounds/playground_single_image.py --evade_method diffuser
+
+python playgrounds/playground_single_image.py --arch gaussian_noise
+python playgrounds/playground_single_image.py --arch gaussian_blur
+python playgrounds/playground_single_image.py --arch jpeg
+python playgrounds/playground_single_image.py --arch brightness
+python playgrounds/playground_single_image.py --arch contrast
+python playgrounds/playground_single_image.py --arch bm3d
