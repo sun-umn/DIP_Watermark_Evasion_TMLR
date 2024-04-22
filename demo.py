af3cd65a957a1caf19261f06d90b582489bf494e
HengyueL <lianghengyue1993@outlook.com>
2024-04-22 16:44:43 -0500

    Update

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 import numpy as np
 import bchlib
-import math
+import math, os
+import pickle
 
 
 def watermark_np_to_str(watermark_np):
@@ -47,57 +48,71 @@ def binary_literal_to_str(watermark_binary_literal):
     return return_str
 
 if __name__ == "__main__":
-    ############### DEMO 1 #######################
-    # # === generate gt watermark in np array ===
-    # watermark_gt = np.random.binomial(1, 0.5, 32)  
-    # print("Watermark Numpy: ", watermark_gt)
+    # ############### DEMO 1 #######################
+    # # # === generate gt watermark in np array ===
+    # # watermark_gt = np.random.binomial(1, 0.5, 32)  
+    # # print("Watermark Numpy: ", watermark_gt)
 
-    # # === convert from np to str ===
-    # watermark_str = watermark_np_to_str(watermark_gt)
-    # print("Watermark String: ", watermark_str)
+    # # # === convert from np to str ===
+    # # watermark_str = watermark_np_to_str(watermark_gt)
+    # # print("Watermark String: ", watermark_str)
 
-    # # === convert from np to binary ===
-    # watermark_utf = watermark_str.encode("utf-8")
-    # print("Watermark machine code: ", watermark_utf)
+    # # # === convert from np to binary ===
+    # # watermark_utf = watermark_str.encode("utf-8")
+    # # print("Watermark machine code: ", watermark_utf)
 
-    # # === convert from binary to string ===
-    # watermark_recovered = watermark_utf.decode("utf-8")
-    # print("Recovered watermark: ", watermark_recovered)
+    # # # === convert from binary to string ===
+    # # watermark_recovered = watermark_utf.decode("utf-8")
+    # # print("Recovered watermark: ", watermark_recovered)
 
-    # # === Further convert it into numpy arr ==
-    # watermark_final = watermark_str_to_numpy(watermark_recovered)
-    # print("Watermark Final: ", watermark_final)
+    # # # === Further convert it into numpy arr ==
+    # # watermark_final = watermark_str_to_numpy(watermark_recovered)
+    # # print("Watermark Final: ", watermark_final)
 
-    # """
-    #     I guess for stegaStamp you need to use <watermark_utf> as the encoder input,
+    # # """
+    # #     I guess for stegaStamp you need to use <watermark_utf> as the encoder input,
 
-    #     and need to convert the decoded message into <watermark_recovered>
-    # """
+    # #     and need to convert the decoded message into <watermark_recovered>
+    # # """
 
 
-    ########### DEMO 2 ##############
-    test_watermark_str = "asdf"   # Suppose you randomly draw a string with length 4. After uft-8 encoding, this will become a 32-bit length array.
-    print("\n Randomly drawn watermark: [{}]  \n".format(test_watermark_str))
+    # ########### DEMO 2 ##############
+    # test_watermark_str = "asdf"   # Suppose you randomly draw a string with length 4. After uft-8 encoding, this will become a 32-bit length array.
+    # print("\n Randomly drawn watermark: [{}]  \n".format(test_watermark_str))
 
-    test_watermark_binary_str = watermark_str_to_bitstr(test_watermark_str)
-    print("Converted Watermark (binary str): ", test_watermark_binary_str)
-    print("  Length: ", len(test_watermark_binary_str), "\n")
+    # test_watermark_binary_str = watermark_str_to_bitstr(test_watermark_str)
+    # print("Converted Watermark (binary str): ", test_watermark_binary_str)
+    # print("  Length: ", len(test_watermark_binary_str), "\n")
 
-    # Sanity check for binary conversion
-    str_recover = binary_literal_to_str(test_watermark_binary_str)
-    print("Sanity check for binary conversion: ", str_recover, "\n")
+    # # Sanity check for binary conversion
+    # str_recover = binary_literal_to_str(test_watermark_binary_str)
+    # print("Sanity check for binary conversion: ", str_recover, "\n")
     
-    utf_code = test_watermark_str.encode("utf-8")
-    # Below is what the stegaStamp source code does: append it to 7-string length but we don't need the appended length since we only need 32 bits.
-    data = bytearray(test_watermark_str + ' '*(7-len(test_watermark_str)), 'utf-8')
-    print("Compare Binary code: ")
-    print("  UTF code: ", utf_code)
-    print("  Stega Stamp UTF code: ", data)  # You can see they are the same binary (within the first 4 strings)
-                                           # You can further look into the diff. between .encode() and bytearray(), but it is irrelevant here.
+    # utf_code = test_watermark_str.encode("utf-8")
+    # # Below is what the stegaStamp source code does: append it to 7-string length but we don't need the appended length since we only need 32 bits.
+    # data = bytearray(test_watermark_str + ' '*(7-len(test_watermark_str)), 'utf-8')
+    # print("Compare Binary code: ")
+    # print("  UTF code: ", utf_code)
+    # print("  Stega Stamp UTF code: ", data)  # You can see they are the same binary (within the first 4 strings)
+    #                                        # You can further look into the diff. between .encode() and bytearray(), but it is irrelevant here.
 
 
-    # === What you need to do is that:
-    # 1) Given a watermarked img ---- I_w
-    # 2) Decode the img and get the decoded str ---  string_decoded = stegaStamp.decode(I_w)
-    # 3) Slice the first 4 characters:               watermark_str_decoded = string_decoded[:4]
-    # 3) Convert the string to binary lateral:       binary_lateral = watermark_str_to_bitstr(watermark_str_decoded)
+    # # === What you need to do is that:
+    # # 1) Given a watermarked img ---- I_w
+    # # 2) Decode the img and get the decoded str ---  string_decoded = stegaStamp.decode(I_w)
+    # # 3) Slice the first 4 characters:               watermark_str_decoded = string_decoded[:4]
+    # # 3) Convert the string to binary lateral:       binary_lateral = watermark_str_to_bitstr(watermark_str_decoded)
+
+    # ##  Test Load data ===
+    dir = os.path.join(
+        ".", "Visualization", "711", "rivaGan", "dip",
+        "vanila", "result.pkl"
+    )
+    with open(dir, 'rb') as f:
+        data = pickle.load(f)
+    
+    print(data.keys())
+    print(len(data["interm_recon"]))
+    print(data["iter_log"])
+
+    print(data["interm_recon"][5])
