befc9b04033a285f201872e919cf8143b8a7c5ec
HengyueL <lianghengyue1993@outlook.com>
2024-04-22 00:09:34 -0500

    Update DIP analysis

--- dip_analysis/dip_denoise.py
+++ dip_analysis/dip_denoise.py
@@ -38,26 +38,32 @@ def main(args):
     img_clean_bgr_uint8_np = cv2.imread(img_clean_path)
     img_clean_bgr_float_np = uint8_to_float(img_clean_bgr_uint8_np)
     # === Add Gaussian Noise ===
-    noise_sigma = 0.15
-    img_noisy_bgr_float_np = random_noise(img_clean_bgr_float_np, mode='gaussian', var=noise_sigma**2)
+    # noise_sigma = 0.15
+    noise_sigma = 0.05
+    # img_noisy_bgr_float_np = random_noise(img_clean_bgr_float_np, mode='gaussian', var=noise_sigma**2)
+    img_noisy_bgr_float_np = random_noise(img_clean_bgr_float_np, mode='s&p', amount=noise_sigma)
+    # Below add a constant bias
+    img_noisy_bgr_float_np = img_noisy_bgr_float_np + 0.1
     img_noisy_bgr_float_np = np.clip(img_noisy_bgr_float_np, 0, 1)
     img_noisy_bgr_uint8_np = float_to_uint8(img_noisy_bgr_float_np)
 
     pure_noise_int_np = img_noisy_bgr_uint8_np.astype(np.int16) - img_clean_bgr_uint8_np.astype(np.int16)
     pure_noise_normed_float = pure_noise_int_np.astype(np.float16)
     value_min, value_max = np.amin(pure_noise_normed_float), np.amax(pure_noise_normed_float)
-    pure_noise_bgr_normed = float_to_uint8((pure_noise_normed_float - value_min) / (value_max - value_min))
     print("Noise value range: Min [{}] - Max [{}]".format(value_min, value_max))
 
 
     # === Compute Energy ===
     img_clean_energy_mean = np.mean((img_clean_bgr_float_np-np.mean(img_clean_bgr_float_np)) ** 2)
     pure_noise_float = (pure_noise_int_np.astype(np.float32)/255.)
-    noise_energy_mean = np.mean((pure_noise_float - np.mean(pure_noise_float))**2)
+    # noise_energy_mean = np.mean((pure_noise_float - np.mean(pure_noise_float))**2)
+    noise_energy_mean = np.mean(pure_noise_float ** 2)
+    noise_var = np.mean((pure_noise_float - np.mean(pure_noise_float))**2)
     print("Mean Energy Comparison: ")
     print("   Img - [{}] | Noise - [{}]".format(img_clean_energy_mean, noise_energy_mean))
-    energy_ratio = img_clean_energy_mean / noise_energy_mean
-    print("   Clean v.s Noise ratio: {:04f}".format(energy_ratio))
+    print("Noise Variance: - [{:.04f}]".format(noise_var))
+    # energy_ratio = img_clean_energy_mean / noise_energy_mean
+    # print("   Clean v.s Noise ratio: {:04f}".format(energy_ratio))
 
     # === Visualize ===
     save_name = os.path.join(vis_root_dir, "img_orig.png")
@@ -67,9 +73,9 @@ def main(args):
 
     # === Init a DIP to denoise the image ====
     device, dtype = torch.device("cuda"), torch.float
-    save_interm = False
+    save_interm = True
     show_every, total_iters = 1, 500
-    lr = 0.01
+    lr = 0.001
     dip_model = get_net_dip().to(device)
     dip_model.train()
     params = dip_model.parameters()
@@ -79,10 +85,9 @@ def main(args):
     # == Optimize ===
     iter_log = []
     mse_clean_log = []
-    mse_noise_log = []
     psnr_clean_log = []
-    psnr_noise_log = []
     mse_noisy_img_log = []
+    psnr_noisy_img_log = []
     recon_interm_log = []
 
     # === === === ===
@@ -101,7 +106,7 @@ def main(args):
             iter_log.append(num_iter)
             img_recon_float_np = tensor_output_to_image_np(net_output)
             img_recon_int_np = float_to_int(img_recon_float_np)
-            img_recon_uint8_np = img_recon_int_np.astype(np.int8)
+            img_recon_uint8_np = img_recon_int_np.astype(np.uint8)
             if save_interm:
                 recon_interm_log.append(img_recon_uint8_np)
 
@@ -114,41 +119,50 @@ def main(args):
                 img_recon_int_np,
                 data_range=255
             )
-            # mse and psnr w.r.t pure noise
-            pure_noise_float = pure_noise_int_np.astype(np.float32) / 255.
-            mse_noise = np.mean((residual_recon - pure_noise_float)**2)
-            psnr_noise = compute_psnr(
-                (residual_recon*255).astype(np.int16),
-                pure_noise_int_np,
-                data_range=(np.amax(pure_noise_int_np)-np.amin(pure_noise_int_np))
-            )
-            # mse 
+
+            # mse & psnr -- noisy image
             mse_noisy_img = np.mean((img_recon_float_np - img_noisy_bgr_float_np)**2)
+            psnr_noisy_img = compute_psnr(
+                img_noisy_bgr_uint8_np,
+                img_recon_uint8_np,
+                data_range=255
+            )
+
             # Log
             mse_clean_log.append(mse_orig)
             psnr_clean_log.append(psnr_orig)
-            mse_noise_log.append(mse_noise)
-            psnr_noise_log.append(psnr_noise)
             mse_noisy_img_log.append(mse_noisy_img)
-
-            print("===== Iter [%d] - Loss [%.06f] =====" % (num_iter, total_loss.item()))
+            psnr_noisy_img_log.append(psnr_noisy_img)
+            # print("===== Iter [%d] - Loss [%.06f] =====" % (num_iter, total_loss.item()))
 
     res = {
         "iter_log": iter_log,
         "mse_clean": mse_clean_log,
         "psnr_clean": psnr_clean_log,
-        "mse_noise": mse_noise_log,
-        "psnr_noise": psnr_noise_log,
-        "mse_noisy_img": mse_noisy_img_log 
+        "mse_noisy_img": mse_noisy_img_log,
+        "psnr_noisy_img": psnr_noisy_img_log
     }
+
+    # Find the iter with smallest clean mse
+    idx = np.argmin(res["mse_clean"])
+    mse_clean = res["mse_clean"][idx]
+    # mse_noise = res["mse_noise"][idx]
+    mse_noisy_img = res["mse_noisy_img"][idx]
+    # recon_ratio = mse_noise / mse_clean
+    print("MSE comparison at best recon. point: ")
+    print("  Clean - [{}] | Noisy Img - [{}]".format(mse_clean, mse_noisy_img))
+    # print("  Ratio: {:04f}".format(recon_ratio))
 
     # === Plot Result ===
     # ITER-MSE curve
     iter_data = res["iter_log"]
     fig, ax = plt.subplots(ncols=1, nrows=1)
-    l1 = ax.plot(iter_data, res["mse_clean"], label="recon v.s clean", alpha=0.5, lw=2)
-    l2 = ax.plot(iter_data, res["mse_noise"], label="(recon - clean) v.s. pure noise", alpha=0.5, lw=2, ls="dashed")
-    l3 = ax.plot(iter_data, res["mse_noisy_img"], label="recon v.s. input (noisy image)", alpha=0.5, lw=2)
+    ax.plot(iter_data, res["mse_clean"], label="recon v.s clean", alpha=0.5, lw=2)
+    # ax.hlines(mse_noisy_img, xmin=0, xmax=np.amax(iter_data), label="Best Recon MSE", ls="dashed")
+    ax.hlines(noise_energy_mean, xmin=0, xmax=np.amax(iter_data), label="Noise Mean Energy (mse)", ls="dashed")
+    ax.hlines(noise_var, xmin=0, xmax=np.amax(iter_data), label="Noise Variance", ls="dashed")
+    ax.plot(iter_data, res["mse_noisy_img"], label="recon v.s. input (noisy image)", alpha=0.5, lw=2)
+    ax.vlines(idx, ymin=0, ymax=np.amax(res["mse_noisy_img"]), label="Best Recon Iter", ls="dashed")
     ax.set_title("Iter - MSE")
     ax.legend()
     plt.tight_layout()
@@ -156,27 +170,27 @@ def main(args):
     plt.savefig(save_name)
     plt.close(fig)
 
-    # 
+    # Iter-PSNR Plot
     fig, ax = plt.subplots(ncols=1, nrows=1)
     l1 = ax.plot(iter_data, res["psnr_clean"], label="recon v.s. clean")
-    l2 = ax.plot(iter_data, res["psnr_noise"], label="(recon - clean) v.s. pure noise")
+    l3 = ax.plot(iter_data, res["psnr_noisy_img"], label="recon v.s. noisy image")
     ax.set_title("Iter - PSNR")
     ax.legend()
     plt.tight_layout()
     save_name = os.path.join(vis_root_dir, "plot-psnr.png")
     plt.savefig(save_name)
     plt.close(fig)
+
+    # === Vis Best Recon ==
+    best_recon_img_bgr = recon_interm_log[idx]
+    save_name = os.path.join(vis_root_dir, "img_best_recon_psnr_{:.04f}.png".format(res["psnr_clean"][idx]))
+    save_image_bgr(best_recon_img_bgr, save_name)
+
+    # === Vis Final Recon ==
+    final_recon_img_bgr = recon_interm_log[-1]
+    save_name = os.path.join(vis_root_dir, "img_final_recon_psnr_{:.04f}.png".format(res["psnr_clean"][-1]))
+    save_image_bgr(final_recon_img_bgr, save_name)
     
-
-    # Find the iter with smallest clean mse
-    idx = np.argmin(res["mse_clean"])
-    mse_clean = res["mse_clean"][idx]
-    mse_noise = res["mse_noise"][idx]
-    recon_ratio = mse_noise / mse_clean
-    print("MSE comparison at best recon. point: ")
-    print("  Clean - [{}] | Noise - [{}]".format(mse_clean, mse_noise))
-    print("  Ratio: {:04f}".format(recon_ratio))
-
 
 if __name__ == "__main__": 
 
