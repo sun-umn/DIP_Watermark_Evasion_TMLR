7df73c5041b19d41294a12d425cd8b45dad0059d
HengyueL <lianghengyue1993@outlook.com>
2024-05-02 16:49:54 -0500

    Update interm result processing

--- msi_experiments/decode_interm_results_hengyue.py
+++ msi_experiments/decode_interm_results_hengyue.py
@@ -15,7 +15,8 @@ sys.path.append(dir_path)
 
 import argparse
 import pickle, os
-
+from watermarkers import get_watermarkers
+from utils.general import watermark_str_to_numpy, watermark_np_to_str
 
 def main(args):
     # === This is where the interm. results are saved ===
@@ -36,10 +37,16 @@ def main(args):
         n_recon = len(img_recon_list)
         print("Total number of interm. recon. to process: [{}]".format(n_recon))
 
-        # === If you need the gt watermark and encoded watermark info ===
+        # === Initiate a encoder & decoder ===
         watermark_gt_str = data_dict["watermark_gt_str"]
-        if watermark_gt_str[1] == "[":  # Some historical none distructive bug :( will cause this reformatting
+        if watermark_gt_str[0] == "[":  # Some historical none distructive bug :( will cause this reformatting
             watermark_gt_str = eval(data_dict["watermark_gt_str"])[0]
+        watermark_gt = watermark_str_to_numpy(watermark_gt_str)
+        watermarker_configs = {
+            "watermarker": args.watermarker,
+            "watermark_gt": watermark_gt
+        }
+        watermarker = get_watermarkers(watermarker_configs)
 
         # Process each inter. recon
         watermark_decoded_log = []  # A list to save decoded watermark
@@ -49,11 +56,11 @@ def main(args):
             # =================== YOUR CODE HERE =========================== #
             
             # Step 0: if you need to change the input format
-            img_input = reformat(img_bgr_uint8)
+            img_input = img_bgr_uint8
 
             # Step 1: Decode the interm. result
             watermark_decoded = watermarker.decode(img_input)
-            watermark_decoded_str = change_to_string_format(watermark_decoded)
+            watermark_decoded_str = watermark_np_to_str(watermark_decoded)
 
             # Step 2: log the result
             watermark_decoded_log.append(watermark_decoded_str)
@@ -62,6 +69,7 @@ def main(args):
         
         # Save the result
         data_dict["watermark_decoded"] = watermark_decoded_log
+        data_dict["watermark_gt_str"] = watermark_gt_str # Some historical none distructive bug :( will cause this reformatting
 
         save_name = os.path.join(save_root_dir, file_name)
         with open(save_name, 'wb') as handle:
@@ -73,7 +81,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Some arguments to play with.')
     parser.add_argument(
         "--watermarker", dest="watermarker", type=str, 
-        help="Specification of watermarking method. [SSL, SteganoGAN, StegaStamp]",
+        help="Specification of watermarking method. [rivaGan, dwtDctSvd]",
         default="rivaGan"
     )
     parser.add_argument(
@@ -83,7 +91,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--evade_method", dest="evade_method", type=str, help="Specification of evasion method.",
-        default="dip"
+        default="vae"
     )
     parser.add_argument(
         "--arch", dest="arch", type=str, 
@@ -96,7 +104,7 @@ if __name__ == "__main__":
                 corrupters --- ["gaussian_blur", "gaussian_noise", "bm3d", "jpeg", "brightness", "contrast"]
                 diffuser --- Do not need.
         """,
-        default="vanila"
+        default="cheng2020-anchor"
     )
     args = parser.parse_args()
     main(args)
