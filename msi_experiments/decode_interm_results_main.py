30c9c9a954fc9b038d0625d088b6e15cc60d9133
HengyueL <lianghengyue1993@outlook.com>
2024-05-07 14:08:22 -0500

    Update large scale experiment result summary

---
+++ msi_experiments/decode_interm_results_main.py
@@ -0,0 +1,181 @@
+"""
+    This script is a skeleton file for **Taihui** to:
+
+    1) Read in the watermark evasion interm. results
+
+    2) Decode each of the interm. result using the encoder/decoder API
+
+    3) Save the result with standardized format
+"""
+import sys, os
+dir_path = os.path.abspath(".")
+sys.path.append(dir_path)
+dir_path = os.path.abspath("..")
+sys.path.append(dir_path)
+
+import argparse, pickle, os, cv2
+import numpy as np
+from skimage.metrics import peak_signal_noise_ratio as compute_psnr
+
+# =====
+from watermarkers import get_watermarkers
+from utils.general import watermark_str_to_numpy, watermark_np_to_str, uint8_to_float
+
+
+def calc_mse(img_1_bgr_uint8, img_2_bgr_uint8):
+    img_1_float = uint8_to_float(img_1_bgr_uint8)
+    img_2_float = uint8_to_float(img_2_bgr_uint8)
+    mse = np.mean((img_1_float - img_2_float)**2)
+    return mse
+
+
+def main(args):
+    # === This is where the interm. results are saved ===
+    data_root_dir = os.path.join("Result-Interm", args.watermarker, args.dataset, args.evade_method, args.arch)
+    file_names = [f for f in os.listdir(data_root_dir) if ".pkl" in f]  # Data are saved as dictionary in pkl format.
+
+    # === This is where the watermarked image is stored ===
+    im_w_root_dir = os.path.join("dataset", args.watermarker, args.dataset, "encoder_img")
+    # === This is where the original clean image is stored ===
+    im_orig_root_dir = os.path.join("dataset", "Clean", args.dataset)
+
+    # === Save the result in a different location in case something went wrong ===
+    save_root_dir = os.path.join("Result-Decoded", args.watermarker, args.dataset, args.evade_method, args.arch)
+    os.makedirs(save_root_dir, exist_ok=True)
+    
+    # === Process each file ===
+    for file_name in file_names:
+        # Retrieve the im_w name
+        im_w_file_name = file_name.replace(".pkl", ".png")
+        if "_hidden" in im_w_file_name:
+            im_orig_name = im_w_file_name.replace("_hidden", "")
+        else:
+            im_orig_name = im_w_file_name
+
+        # Readin the intermediate files
+        data_file_path = os.path.join(data_root_dir, file_name)
+        with open(data_file_path, 'rb') as handle:
+            data_dict = pickle.load(handle)
+        # Readin the im_w into bgr uint8 format
+        im_w_path = os.path.join(im_w_root_dir, im_w_file_name)
+        im_w_bgr_uint8 = cv2.imread(im_w_path)
+        # Readin the 
+        im_orig_path = os.path.join(im_orig_root_dir, im_orig_name)
+        im_orig_bgr_uint8 = cv2.imread(im_orig_path)
+        
+        # Get the reconstructed data from the interm. result
+        img_recon_list = data_dict["interm_recon"]  # A list of recon. image in "bgr uint8 np" format (cv2 standard format)
+        n_recon = len(img_recon_list)
+        print("Total number of interm. recon. to process: [{}]".format(n_recon))
+
+        # === Initiate a encoder & decoder ===
+        watermark_gt_str = data_dict["watermark_gt_str"]
+        if watermark_gt_str[0] == "[":  # Some historical none distructive bug :( will cause this reformatting
+            watermark_gt_str = eval(data_dict["watermark_gt_str"])[0]
+        watermark_gt = watermark_str_to_numpy(watermark_gt_str)
+        watermarker_configs = {
+            "watermarker": args.watermarker,
+            "watermark_gt": watermark_gt
+        }
+        watermarker = get_watermarkers(watermarker_configs)
+
+        # Process each inter. recon
+        watermark_decoded_log = []  # A list to save decoded watermark
+        index_log = data_dict["index"]
+        psnr_orig_log = []
+        mse_orig_log = []
+        psnr_w_log = []
+        mse_w_log = []
+        for img_idx in range(n_recon):
+            img_bgr_uint8 = img_recon_list[img_idx]    # shape [512, 512, 3]
+            if args.watermarker == "StegaStamp" and args.arch in ["cheng2020-anchor", "mbt2018"]:
+                img_bgr_uint8 = cv2.resize(img_bgr_uint8, (400, 400), interpolation=cv2.INTER_LINEAR)
+
+            # =================== YOUR CODE HERE =========================== #
+            
+            # Step 0: if you need to change the input format
+            img_input = img_bgr_uint8
+
+            # Step 1: Decode the interm. result
+            watermark_decoded = watermarker.decode(img_input)
+            watermark_decoded_str = watermark_np_to_str(watermark_decoded)
+
+            # Step 2: log the result
+            watermark_decoded_log.append(watermark_decoded_str)
+
+            # ============================================================= #
+
+            # Calculate the quality: mse and psnr
+            mse_recon_orig = calc_mse(im_orig_bgr_uint8, img_bgr_uint8)
+            mse_recon_w = calc_mse(im_w_bgr_uint8, img_bgr_uint8)
+
+            psnr_recon_orig = compute_psnr(
+                im_orig_bgr_uint8.astype(np.int16), img_bgr_uint8.astype(np.int16), data_range=255
+            )
+            psnr_recon_w = compute_psnr(
+                im_w_bgr_uint8.astype(np.int16), img_bgr_uint8.astype(np.int16), data_range=255
+            )
+            mse_orig_log.append(mse_recon_orig)
+            mse_w_log.append(mse_recon_w)
+            psnr_orig_log.append(psnr_recon_orig)
+            psnr_w_log.append(psnr_recon_w)
+
+        # Save the result
+        processed_dict = {
+            "index": index_log,
+            "watermark_gt_str": watermark_gt_str, # Some historical none distructive bug :( will cause this reformatting
+            "watermark_decoded": watermark_decoded_log,
+            "mse_orig": mse_orig_log,
+            "psnr_orig": psnr_orig_log,
+            "mse_w": mse_w_log,
+            "psnr_w": psnr_w_log
+        }
+
+        save_name = os.path.join(save_root_dir, file_name)
+        with open(save_name, 'wb') as handle:
+            pickle.dump(processed_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print("Decoded Interm. result saved to {}".format(save_name))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Some arguments to play with.')
+    parser.add_argument(
+        "--watermarker", dest="watermarker", type=str, 
+        help="Specification of watermarking method. [rivaGan, dwtDctSvd]",
+        default="dwtDctSvd"
+    )
+    parser.add_argument(
+        "--dataset", dest="dataset", type=str, 
+        help="Dataset [COCO, DiffusionDB]",
+        default="COCO"
+    )
+    # parser.add_argument(
+    #     "--evade_method", dest="evade_method", type=str, help="Specification of evasion method.",
+    #     default="vae"
+    # )
+    # parser.add_argument(
+    #     "--arch", dest="arch", type=str, 
+    #     help="""
+    #         Secondary specification of evasion method (if there are other choices).
+
+    #         Valid values a listed below:
+    #             dip --- ["vanila", "random_projector"],
+    #             vae --- ["cheng2020-anchor", "mbt2018", "bmshj2018-factorized"],
+    #             corrupters --- ["gaussian_blur", "gaussian_noise", "bm3d", "jpeg", "brightness", "contrast"]
+    #             diffuser --- Do not need.
+    #     """,
+    #     default="cheng2020-anchor"
+    # )
+    args = parser.parse_args()
+    
+    root_lv1 = os.path.join("Result-Interm", args.watermarker, args.dataset)
+    corrupter_names = [f for f in os.listdir(root_lv1)]
+    for corrupter in corrupter_names:
+        root_lv2 = os.path.join(root_lv1, corrupter)
+        arch_names = [f for f in os.listdir(root_lv2)]
+        for arch in arch_names:
+            args.evade_method = corrupter
+            args.arch = arch
+            print("Processing: {} - {} - {} - {}".format(args.watermarker, args.dataset, args.evade_method, args.arch))
+            main(args)
+    print("\n***** Completed. *****\n")
--- msi_experiments/process_decoded_data.py
+++ msi_experiments/process_decoded_data.py
@@ -7,29 +7,156 @@ sys.path.append(dir_path)
 
 import argparse, pickle, os, cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme()
+from skimage.metrics import peak_signal_noise_ratio as compute_psnr
+
+
+def calc_bitwise_acc(gt_str, decoded_str):
+    correct, total = 0., 0.
+    for i in range(len(gt_str)):
+        if gt_str[i] == decoded_str[i]:
+            correct = correct + 1.
+        total = total + 1.
+    return correct / total
 
 
 def main(args):
     data_root_dir = os.path.join("Result-Decoded", args.watermarker, args.dataset, args.evade_method, args.arch)
-    file_path = os.path.join(data_root_dir, "Img-50.pkl")
+    file_names = [f for f in os.listdir(data_root_dir) if ".pkl" in f]
 
-    # Load Data
-    with open(file_path, 'rb') as handle:
-        data_dict = pickle.load(handle)
+    # path to get im_clean and im_w
+    im_w_root_dir = os.path.join("dataset", args.watermarker, args.dataset, "encoder_img")
+    im_orig_root_dir = os.path.join("dataset", "Clean", args.dataset)
 
-    a_gt = data_dict["watermark_gt_str"]
-    aa = data_dict["watermark_decoded"]
-    bb = data_dict["psnr_w"]
+    # === Create Save directory ===
+    save_root_dir = os.path.join("Result-Stats-Summary", args.watermarker, args.dataset, args.evade_method)
+    os.makedirs(save_root_dir, exist_ok=True)
+    save_file_name = os.path.join(save_root_dir, "{}.pkl".format(args.arch))
 
-    a = aa[-2]
-    log = []
-    for i in range(len(a_gt)):
-        if a_gt[i] == a[i]:
-            log.append(1)
+    # Logs to save after processing
+    psnr_w_to_orig_log = []
+    best_psnr_w_log = []
+    best_psnr_orig_log = []
+    evade_success_log = []
+    
+    # Collect all interm. results and calc. necessary metrics
+    for file_name in file_names:
+        file_path = os.path.join(data_root_dir, file_name)
+
+        
+
+        # (1) Retrieve the im_w name
+        im_w_file_name = file_name.replace(".pkl", ".png")
+        if "_hidden" in im_w_file_name:
+            im_orig_name = im_w_file_name.replace("_hidden", "")
         else:
-            log.append(0)
-    bit_acc = np.mean(log)
-    print()
+            im_orig_name = im_w_file_name
+        # Readin the im_w into bgr uint8 format
+        im_w_path = os.path.join(im_w_root_dir, im_w_file_name)
+        im_w_bgr_uint8 = cv2.imread(im_w_path)
+        # Readin im_orig and im_w and calculate this psnr 
+        im_orig_path = os.path.join(im_orig_root_dir, im_orig_name)
+        im_orig_bgr_uint8 = cv2.imread(im_orig_path)
+        psnr_w_to_orig = compute_psnr(
+            im_orig_bgr_uint8.astype(np.int16), im_w_bgr_uint8.astype(np.int16), data_range=255
+        )
+        # save (1)
+        psnr_w_to_orig_log.append(psnr_w_to_orig)
+
+        # Load Data
+        with open(file_path, 'rb') as handle:
+            data_dict = pickle.load(handle)
+        
+        index_log = data_dict["index"]
+        watermark_gt_str = data_dict["watermark_gt_str"]
+        watermark_decoded_str = data_dict["watermark_decoded"]
+        mse_orig_log = data_dict["mse_orig"]
+        mse_w_log = data_dict["mse_w"]
+        psnr_orig_log = data_dict["psnr_orig"]
+        psnr_w_log = data_dict["psnr_w"]
+
+        num_interm_data = len(psnr_w_log)
+        print("Total number of interm data: {}".format(num_interm_data))
+
+        # === Calc bitwise acc ===
+        bitwise_acc_log = []
+        for i in range(num_interm_data):
+            bitwise_acc = calc_bitwise_acc(watermark_gt_str, watermark_decoded_str[i])
+            bitwise_acc_log.append(bitwise_acc)
+
+        # === To finde the best evade Iter ===
+        best_index, best_psnr_w, best_psnr_orig = None, -float("inf"), None
+        detection_threshold = 0.75
+        for idx in range(num_interm_data-1, -1, -1):
+            psnr_w = psnr_w_log[idx]
+            psnr_orig = psnr_orig_log[idx]
+            bitwise_acc = bitwise_acc_log[idx]
+
+            condition_1 = (1-detection_threshold) < bitwise_acc < detection_threshold
+            condition_2 = psnr_w > best_psnr_w
+            if condition_1 and condition_2:
+                best_index = index_log[idx]
+                best_psnr_w = psnr_w
+                best_psnr_orig = psnr_orig
+        print("Best evade iter [{}] with PSNR-W [{}] & PSNR-orig [{}]".format(best_index, best_psnr_w, best_psnr_orig))
+
+        if best_index is None:
+            print("Does not evade successfully")
+            # best_psnr_w_log = []
+            # best_psnr_orig_log = []
+            evade_success_log.append(0)
+        else:
+            best_psnr_w_log.append(best_psnr_w)
+            best_psnr_orig_log.append(best_psnr_orig)
+            evade_success_log.append(1)
+
+        # # === Sanity Check === Plot the psnr and bitwise acc curve
+        # fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
+        # ax[0].plot(index_log, psnr_orig_log, label="PSNR (recon - clean)", color="orange")
+        # ax[0].plot(index_log, psnr_w_log, label="PSNR (recon - watermarked)", color="blue", ls="dashed")
+        # if best_index is not None:
+        #     ax[0].vlines(best_index, ymin=np.amin(psnr_w_log), ymax=np.amax(psnr_w_log), color="black", ls="dashed", label="Best Evade Iter")
+        # ax[0].legend()
+
+        # ax[1].plot(index_log, bitwise_acc_log, label="Bitwise Acc.")
+        # ax[1].hlines(y=detection_threshold, xmin=np.amin(index_log), xmax=np.amax(index_log), ls="dashed", color="black")
+        # ax[1].hlines(y=(1-detection_threshold), xmin=np.amin(index_log), xmax=np.amax(index_log), ls="dashed", color="black")
+        # if best_index is not None:
+        #     ax[1].vlines(best_index, ymin=0, ymax=1, color="black", ls="dashed", label="Best Recon Iter")
+        # ax[1].legend()
+        # plt.tight_layout()
+        # # save_name = os.path.join(save_root, "psnr_bt_acc.png")
+        # # plt.savefig(save_name)
+        # plt.show()
+        # plt.close(fig)
+        
+    # === Summarize Data ===
+    mean_psnr_w, std_psnr_w = np.mean(best_psnr_w_log), np.std(best_psnr_w_log)
+    mean_psnr_orig, std_psnr_orig = np.mean(best_psnr_orig_log), np.std(best_psnr_orig_log)
+    mean_psnr_w_to_orig, std_psnr_w_to_orig = np.mean(psnr_w_to_orig_log), np.std(psnr_w_to_orig_log)
+    evade_success_rate = np.mean(evade_success_log)
+    print("===== Processed Summary: Watermarker [{}] - Dataset [{}] =====".format(args.watermarker, args.dataset))
+    print("PSNR ot orig: Mean {:03f} - std({:03f})".format(mean_psnr_w_to_orig, std_psnr_w_to_orig))
+    print("Best PSNR-W: Mean - STD: ")
+    print("  [{:03f}] - [{:03f}]".format(mean_psnr_w, std_psnr_w))
+    print("Best PSNR-Orig: Mean - STD: ")
+    print("  [{:03f}] - [{:03f}]".format(mean_psnr_orig, std_psnr_orig))
+    print("Evasion success rate: {:.03f} %".format(evade_success_rate * 100))
+
+    # === Save processed data ===
+    save_dict = {
+        "psnr_w_to_orig": psnr_w_to_orig_log,
+        "best_psnr_w": best_psnr_w_log,
+        "best_psnr_orig": best_psnr_orig_log,
+        "evade_success_rate": np.mean(evade_success_log)
+    }
+    with open(save_file_name, 'wb') as handle:
+        pickle.dump(save_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print("Decoded Interm. result saved to {}".format(save_file_name))
+        
+        
 
 
 if __name__ == "__main__":
@@ -46,7 +173,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--evade_method", dest="evade_method", type=str, help="Specification of evasion method.",
-        default="vae"
+        default="corrupters"
     )
     parser.add_argument(
         "--arch", dest="arch", type=str, 
@@ -59,8 +186,19 @@ if __name__ == "__main__":
                 corrupters --- ["gaussian_blur", "gaussian_noise", "bm3d", "jpeg", "brightness", "contrast"]
                 diffuser --- Do not need.
         """,
-        default="cheng2020-anchor"
+        default="jpeg"
     )
     args = parser.parse_args()
     main(args)
+
+    # root_lv1 = os.path.join("Result-Decoded", args.watermarker, args.dataset)
+    # corrupter_names = [f for f in os.listdir(root_lv1)]
+    # for corrupter in corrupter_names:
+    #     root_lv2 = os.path.join(root_lv1, corrupter)
+    #     arch_names = [f for f in os.listdir(root_lv2)]
+    #     for arch in arch_names:
+    #         args.evade_method = corrupter
+    #         args.arch = arch
+    #         print("Processing: {} - {} - {} - {}".format(args.watermarker, args.dataset, args.evade_method, args.arch))
+    #         main(args)
     print("Completed.")
