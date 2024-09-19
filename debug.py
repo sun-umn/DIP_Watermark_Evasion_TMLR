6f844a54f59a542a9ab1fc9ed02e6e9cb3cf6ab8
HengyueL <lianghengyue1993@outlook.com>
2024-09-18 22:36:22 -0700

    Update

--- debug.py
+++ debug.py
@@ -1,15 +1,17 @@
 import os
 import pickle
+import matplotlib.pyplot as plt
+import cv2
 
 
 if __name__ == "__main__":
     file_dir = os.path.join(
-        "Result-Decoded", "TrustMark", "COCO",
-        "vae", "cheng2020-anchor", "Img-1.pkl"
+        "Img-77.pkl"
     )
     with open(file_dir, 'rb') as handle:
         data_dict = pickle.load(handle)
 
-    print(data_dict.keys())
-    for key in data_dict.keys():
-        print("{} - {}".format(key, data_dict[key]))
+    interm_recon = data_dict["interm_recon"][0]
+    plt.imshow(interm_recon)
+    plt.show()
+    print()
